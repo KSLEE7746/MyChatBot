9a74f1fec2256731b51cc9faa0a9191884d86172
kslee7746 <kslee7746@naver.com>
2017-08-29 10:37:43 +0900

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from microsoftbotframework import ReplyToActivity
+import requests
+import json
+
+def echo_response(message):
+  print(message)
+  
+  if message["type"] == "message":
+    if "카드" in message["text"]:
+      msg = "아직 서비스 준비 중 입니다."
+    elif "비교" in message["text"]:
+      msg = "아직 서비스 준비중이라니까?"
+    elif "안녕" in message["text"]:
+      msg = "안녕하세요."
+    else:
+      msg = "명령어 - 카드, 비교, 안녕"
+
+    ReplyToActivity(fill=message, text=msg).send()
