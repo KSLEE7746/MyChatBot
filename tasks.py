2f9efb9736d3774a7ae083541a33c2e73537be4d
kslee7746 <kslee7746@naver.com>
2017-08-29 17:53:44 +0900

    konlpyTest

--- tasks.py
+++ tasks.py
@@ -1,11 +1,19 @@
 from microsoftbotframework import ReplyToActivity
 import requests
 import json
+from konlpy.tag import Twitter
+from konlpy.tag import Komoran
+
+komoran = Komoran()
+
+def konlpy_parse(text):
+  return komoran.nouns(text)
 
 def echo_response(message):
   print(message)
   
   if message["type"] == "message":
+    print(konlpy_parse(message["type"]))
     if "카드" in message["text"]:
       msg = "카드 서비스 준비 중 입니다."
     elif "비교" in message["text"]:
