78068c9ca0b56fcfe22bad2a832318f6135da8c2
kslee7746 <kslee7746@naver.com>
2017-09-01 11:31:18 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from microsoftbotframework import ReplyToActivity
 import requests
 import json
+import socket 
+import sys 
 
 def echo_response(message):
   print(message)
@@ -13,5 +15,21 @@ def echo_response(message):
       msg = "안녕하세요."
     else:
       msg = "응답할 수 없음"
-
+      
+    serverIP = '52.79.99.215'
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # SOCK_STREAM은 TCP socket을 뜻함 
+    # sock.bind((bindIP, 1)) 
+    try: 
+      sock.connect((serverIP, 12222)) 
+    # 서버에 연결 요청 
+    # 서버로 송신 
+      sbuff = bytes(msg, encoding='utf-8') 
+      sock.send(sbuff) # 메시지 송신 
+      print('송신 {0}'.format(msg)) # 서버로부터 수신 
+      rbuff = sock.recv(1024) # 메시지 수신 
+      received = str(rbuff, encoding='utf-8') 
+      print('수신 : {0}'.format(received)) 
+    finally: socket.close()
+      
     ReplyToActivity(fill=message, text=msg).send()
