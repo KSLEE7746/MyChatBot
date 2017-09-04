b5ca3b53ee942c23bcb2b178b38f673f82669224
kslee7746 <kslee7746@naver.com>
2017-09-04 11:31:16 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -16,8 +16,8 @@ def echo_response(message):
       msg = "응답할 수 없음"
   
     s = socket.socket()
-    #host = '52.79.99.215'
-    host = '222.106.22.63'
+    host = '13.124.234.183'
+    # host = '222.106.22.63'
     port = 12222
 
     s.connect((host, port))
