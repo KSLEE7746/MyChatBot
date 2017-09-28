82ea880be4cbb5e07a424594b087ac6ca1058583
kslee7746 <kslee7746@naver.com>
2017-09-28 17:14:55 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -11,8 +11,8 @@ def echo_response(message):
     user_id = message["from"]["id"]
     send_msg = message["text"] +"|"+ user_id
     s = socket.socket()
-#     host = '52.79.221.214'
-    host = '222.106.22.63'
+    host = '13.124.121.245'
+    # host = '222.106.22.63'
     port = 12222
 
     s.connect((host, port))
@@ -36,7 +36,7 @@ def echo_response(message):
 #       msg = "응답할 수 없음"
   elif message["type"] == "conversationUpdate":
     if "card_bot_test" in message["membersAdded"][0]["id"]: 
-      send_msg = "카드 추천 챗봇 입니다.\n1. OO 신용/체크카드 추천\n2. 내 카드 / OO카드 혜택 알려줘\n3. OO카드와 XX카드 비교해줘\n4. OO카드 혜택 검색\n5. 주변 카페/편의점/영화관/패스트푸드 찾아줘\n"
+      send_msg = "카드 추천 챗봇 입니다.\n1. OO 신용/체크카드 추천\n2. 내 카드/OO카드 혜택 알려줘\n3. OO카드 혜택 검색\n4. 저번달/이번달 소비내역 알려줘\n5. 주변 카페/편의점/영화관/패스트푸드 찾아줘\n"
       ReplyToActivity(fill=message, text=send_msg).send()
     else:
       pass
