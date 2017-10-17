1192a4239ee49f09b0c75e1a630a545c8b572fa0
kslee7746 <kslee7746@naver.com>
2017-10-17 14:04:31 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -36,7 +36,7 @@ def echo_response(message):
 #       msg = "응답할 수 없음"
   elif message["type"] == "conversationUpdate":
     if "card_bot_test" in message["membersAdded"][0]["id"]: 
-      send_msg = "카드 추천 챗봇 입니다.\n\n아래와 같은 형식으로 입력해주세요.\n\n▶ 하나 신용/체크카드 추천해줘\n\n▶ 나한테 맞는 카드 추천해줘\n\n▶ 내 카드/○○카드 혜택 알려줘\n\n▶ □□카드랑 △△카드 비교해줘\n"
+      send_msg = "카드 추천 챗봇 입니다.\n\n아래와 같은 형식으로 입력해주세요.\n\n▶ 하나 신용/체크카드 추천해줘\n\n▶ 나한테 맞는 카드 추천해줘\n\n▶ 내 카드/○○카드 혜택 알려줘\n\n▶ ◇◇카드랑 △△카드 비교해줘\n"
       ReplyToActivity(fill=message, text=send_msg).send()
     else:
       pass
